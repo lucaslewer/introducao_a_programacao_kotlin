ca6e9c16e3edd59ae80997d6be2ec0e0e31acfd6
Lucas Lewer <130666580+lucaslewer@users.noreply.github.com>
2024-02-28 23:03:24 -0300

    Add files via upload

---
+++ teste.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val firstNumber = 10
+    val secondNumber = 5
+    val thirdNumber = 8
+    
+    val result = add(firstNumber, secondNumber)
+    val anotherResult = add(firstNumber, thirdNumber)
+
+    println("$firstNumber + $secondNumber = $result")
+    println("$firstNumber + $thirdNumber = $anotherResult")
+}
+
+fun add(a: Int, b: Int): Int {
+    return a + b
+}
